61f69b10bd658ee9a8bbd079c024ba76e8346018
Julian Tabel <jtabel@bigpoint.net>
2013-01-04 13:31:23 +0100

    single to double quotes

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,12 @@ description				"Installs racktables DC asset management system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version					"0.0.1"
 
-depends 				"apache2"
+recipe "racktables", "Installs racktables DC asset management system"
+
+depends					"apache2"
 depends					"percona"
 
 %w{ debian }.each do |os|
 	supports os
 end
 
-recipe "racktables::default", "Installs racktables DC asset management system"

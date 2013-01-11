3e3ce896a4e4d8d8f258dc5938dd5719dc69520c
Julian Tabel <jtabel@bigpoint.net>
2013-01-11 16:18:07 +0100

    increase version number to 0.0.6

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email		"jtabel@bigpoint.net"
 license					"All rights reserved"
 description				"Installs racktables DC asset management system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.5"
+version           "0.0.6"
 
 recipe "racktables", "Installs racktables DC asset management system"
 

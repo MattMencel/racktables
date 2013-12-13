e73524e69c9e70afba5330315b36e5f068957817
Julian Tabel <j.tabel@bigpoint.net>
2013-12-13 15:45:33 +0100

    removed supported OSes, should work everywhere. version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email		"j.tabel@bigpoint.net"
 license					"All rights reserved"
 description				"Installs racktables DC asset management system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.6"
+version           "0.1.0"
 
 recipe "chef-racktables", "Installs racktables DC asset management system"
 

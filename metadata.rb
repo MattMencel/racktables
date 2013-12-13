79f664b86534b39ed42e2b66f5b2a6a3de5a4a98
Julian Tabel <j.tabel@bigpoint.net>
2013-12-13 18:01:45 +0100

    removed supported os, should work everywhere

--- metadata.rb
+++ metadata.rb
@@ -1,21 +1,18 @@
-name					"chef-racktables"
-maintainer				"Bigpoint GmbH"
-maintainer_email		"j.tabel@bigpoint.net"
-license					"All rights reserved"
-description				"Installs racktables DC asset management system"
+name                    "chef-racktables"
+maintainer              "Bigpoint GmbH"
+maintainer_email        "j.tabel@bigpoint.net"
+license                 "All rights reserved"
+description             "Installs racktables DC asset management system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version                 "0.1.0"
 
 recipe "chef-racktables", "Installs racktables DC asset management system"
 
-depends					"apache2"
-depends					"git"
+depends                 "apache2"
+depends                 "git"
 depends                 'database', ">= 1.5.3"
 depends                 'mysql', "= 3.0.12"
 depends                 "build-essential"
 
 
-%w{ debian, ubuntu }.each do |os|
-	supports os
-end
 
